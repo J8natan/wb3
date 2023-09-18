02fe6e742a112dd4ba6d67f4573253f5db786b28
J8natan <djhonne@hotmail.com>
2023-09-17 22:18:51 -0300

    curso completo

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,68 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import CreateProduct from "../components/CreateProduct";
+import Product from "../components/Product";
 import HeadComponent from '../components/Head';
 
-// Constants
-const TWITTER_HANDLE = "web3dev_";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+// Constantes
+const TWITTER_HANDLE = "djhonne";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
-  
+  const { publicKey } = useWallet();
+  const isOwner = ( publicKey ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY : false );
+  const [creating, setCreating] = useState(false);
+  const [products, setProducts] = useState([]);
+ 
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://lh3.googleusercontent.com/gnEmm41ofBbB88A11jlDHYvFuL02OylJCmzHWhVeHOA-IjIYgp1usI7t-u8iOGEzRXFeAF4J-hN9jk6_ypsIFVRX5FGQ5-7324H_BliMk-kI0E9nf0pAtgD3ItF3WP4L0UwWSkt_Py6UD4Iei7Hxm2s1N0-w_nd90BjrRRTrpKGP20_udB4" alt="emoji" />
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>    
+    </div>
+  );
+ 
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Produtos", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
   return (
     <div className="App">
       <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Loja de emojis 😈</p>
-          <p className="sub-text">A única loja de emojis que aceita shitcoins</p>
+          <p className="header">  Concessionaria Espacial</p>
+          <p className="sub-text">A primeira Concessionaria Espacial</p>
+
+          {isOwner && (
+            <button className="create-product-button" onClick={() => setCreating(!creating)}>
+              {creating ? "Close" : "Criar Produto"}
+            </button>
+          )}
         </header>
 
         <main>
-          <img className='gif-image' src="https://media.tenor.com/1mJ-tJSzvwsAAAAd/solana-sol.gif" alt="emoji" />
+          {creating && <CreateProduct />}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
@@ -28,11 +72,11 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`contruido na @${TWITTER_HANDLE}`}</a>
+          >{`contruido por @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
   );
 };
 
-export default App;
+export default App;  
